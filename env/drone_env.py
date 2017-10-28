ee81789f64394a007999fd7a3f9d1e594fe11ece
Zeyuan Jin <zeyuan.1.jin@gmail.com>
2017-10-28 18:44:16 -0400

    Update drone_env.py

--- env/drone_env.py
+++ env/drone_env.py
@@ -8,7 +8,7 @@ class env():
 
         def _step(self, a):
             """ Input:
-                Actions: np.array 
+                Actions: np.array
                 Output:
                 Observation: np.array (depth image, rgb image or other useful image);
                 Reward: float (every move takes -1 reward, colipse takes -20 or other,
@@ -17,4 +17,23 @@ class env():
                 if_done: boolean if finished
                 info: Environment info for debug, not square"""
 
-                
+        def _get_image(self, if_depth):
+            """
+            Be called in step
+            Input, if_depth: boolean, determine if depth image taken
+            Output, image taken from the api
+            """
+
+        def _reset(self):
+            """
+            Reset the drone position to the fixed point
+            Replace the target object
+            """
+
+        def _render(self, mode, close):
+            # Possilbly not useful
+            # Way to record the image or video may be directly in the api
+
+        # Keys to actions, to explain the meaning for actions
+
+        # Clone and restore state, check the reference file at the beginning
